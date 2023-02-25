9b9e7edc2e14497887a08c490ebfb77abff5089f
Manisha-patibandla <manishapatibandla1@gmail.com>
2023-02-25 13:57:55 +0530

    first commit

---
+++ MatchesOutput.java
@@ -0,0 +1,16 @@
+[India Vs Australia, India Vs England, India Vs Pakistan, India Vs singapore, Australia Vs England, Australia Vs Pakistan, Australia Vs singapore, England Vs Pakistan, England Vs singapore, Pakistan Vs singapore]
+India
+4
+0
+Australia
+2
+2
+England
+3
+1
+Pakistan
+0
+4
+singapore
+1
+3
